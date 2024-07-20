6a9813c1f8936a84c3522f0b4d804861a188cda0
vlladimirr21 <vlladimirr21@gmail.com>
2024-07-20 17:02:23 +0300

    for styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,9 @@
 <style>
+
+	body {
+  	background-color: #f7f7f7; /* A soft, neutral gray background color */
+	}
+	
 	main {
 		text-align: center;
 		padding: 1em;
@@ -18,4 +23,60 @@
 			max-width: none;
 		}
 	}
+
+
+.currency-converter {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
+.currency-converter label {
+  width: 45%;
+  margin-right: 10px;
+}
+
+.currency-converter input[type='number'] {
+  width: 45%;
+  margin-right: 10px;
+}
+
+.currency-converter select {
+  width: 45%;
+  margin-right: 10px;
+}
+
+.currency-converter button {
+  background-color: #337ab7;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 2px;
+  cursor: pointer;
+}
+
+.currency-converter button:hover {
+  background-color: #23527c;
+}
+
+.currency-converter button:disabled {
+  background-color: #ccc;
+  color: #666;
+  cursor: not-allowed;
+}
+
+.result {
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 20px;
+}
+
+.result span {
+  color: #337ab7;
+}
+
+</style>
+<style global>
+  @import './global.css';
 </style>
